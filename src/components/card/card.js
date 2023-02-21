a04314e17a2ccfe2a83985080e1295297a665b92
Gabriel Molonhoni <bebetete1122@gmail.com>
2023-02-21 15:40:54 -0300

    updated card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,26 +4,34 @@ function Card() {
   return (
     <div className="card">
       <div className="container">
-        <div className="body">
-          <div className="stock_img">
-            <img src="./images/itsa.png"></img>
-          </div>
+        <div className="body">          
           <div className="title">
-            <div className="stock_ticker">ITSA</div>
-            <div className="stock_name">Itausa S.A</div>
+            <div className="stock_img">
+              <img src="./images/itsa.png"></img>
+            </div>
+            <div className="stock_name">
+              <div className="stock_ticker">ITSA</div>
+              <div className="stock_description">Itausa S.A</div>
+
+            </div>
+            
           </div>
-          <div className="current_value">
-            <p>Valor Atual</p>
-            <span>R$ 10,00</span>
+
+          <div className="values">
+            <div className="current_value">
+              <p>Valor Atual</p>
+              <span>R$ 10,00</span>
+            </div>
+            <div className="ideal_value">
+              <p>Valor Ideal</p>
+              <span>R$ 15,00</span>
+            </div>
+            <div className="upside_value">
+              <p>Valor Atual</p>
+              <span>50%</span>
+            </div>
           </div>
-          <div className="ideal_value">
-            <p>Valor Ideal</p>
-            <span>R$ 15,00</span>
-          </div>
-          <div className="upside_value">
-            <p>Valor Atual</p>
-            <span>50%</span>
-          </div>
+          
         </div>
       </div>
     </div>    
